fd5314f0d30b9c3483090bb73162dac7e727908f
zhaobao1830 <373098777@qq.com>
2023-03-20 17:29:51 +0800

    修改全局配置文件

--- src/core/config/index.js
+++ src/core/config/index.js
@@ -1,7 +1,6 @@
 // 全局配置
 const config = {
-  baseUrl: process.env.VUE_APP_BASE_URL || '/api/',
-  stagnateTime: 60 * 60 * 1000 // 无操作停滞时间  默认1小时
+  baseUrl: process.env.VUE_APP_BASE_URL || '/api/'
 }
 
 export default config
