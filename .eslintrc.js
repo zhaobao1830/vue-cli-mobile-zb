d328f0d3720a9b9460cc0025ac7523619fff2629
zhaobao1830 <373098777@qq.com>
2023-04-13 11:39:25 +0800

    修改eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,22 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
+    es2021: true,
     node: true
   },
   extends: [
     'plugin:vue/essential',
-    '@vue/standard'
+    'eslint:recommended'
   ],
-  parserOptions: {
-    parser: '@babel/eslint-parser'
-  },
+  parser: 'vue-eslint-parser',
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    "space-before-function-paren": 0, // 忽略空格
-    "vue/script-indent": [ // script区域缩进俩格
-      "error",
+    'space-before-function-paren': 0, // 忽略空格
+    semi: ['error', 'never'], // 无分号
+    'vue/script-indent': [ // script区域缩进俩格
+      'error',
       2,
       {
         // script标签缩进设置
@@ -24,13 +25,14 @@ module.exports = {
         ignores: []
       }
     ],
-    indent: 0,
-    semi: ["error", "never"], // 无分号
-    'camelcase': 0, // 忽略强制驼峰命名
-    "no-trailing-spaces": 0, // 忽略语句后面出现的空格
-    "no-use-before-define": 'off',
-    "vue/multi-word-component-names": ["error", {
-      "ignores": ['loading', 'home', 'login', 'ruler', 'message']
-    }]
+    quotes: ['error', 'single'], // 强制使用单引号
+    indent: 'off',
+    camelcase: 0, // 忽略强制驼峰命名
+    'no-trailing-spaces': 0, // 忽略语句后面出现的空格
+    'no-mixed-spaces-and-tabs': 0,
+    'no-tabs': 'off',
+    'no-use-before-define': 'off',
+    // 关闭名称校验
+    'vue/multi-word-component-names': 'off'
   }
 }
